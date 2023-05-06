cba0b70e9b8279afb62b8da29244f074eaa50371
salma Elmaghawry <112850475+salma-elmaghawry@users.noreply.github.com>
2023-05-06 18:47:54 +0300

    Add files via upload

---
+++ Project5/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream >
+using namespace std; 
+int main()
+{
+	int n;
+	cin >> n;
+	for (int  i = 0; i < n; i++)
+	{
+		int  num;
+		cin >> num;
+		
+			cout << (num /3) - 2 << endl;
+	}
+	return 0;
+}
